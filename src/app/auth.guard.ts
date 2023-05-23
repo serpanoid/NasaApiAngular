0063101cffb3e080bdac256c8f01f5f5619e6f0b
Nykyta <cloudfreze102@gmail.com>
2023-05-23 23:18:23 +0300

    Updated Angular CLI and plugins

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SharedService } from './shared.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private router: Router, private sharedService: SharedService) {}
 
   canActivate(
